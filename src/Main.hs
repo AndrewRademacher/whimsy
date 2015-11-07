ee3205c4d4cdae82b3eab8dea5516974557c5e96
AndrewRademacher <andrewrademacher@gmail.com>
2015-11-07 17:49:35 -0600

    IORef implementation 1.

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators     #-}
 
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Either
 import           Data.Aeson
 import           Data.Aeson.Casing
@@ -21,7 +22,8 @@ type Email = Text
 
 type API
     =    "player" :> ReqBody '[JSON] NewPlayer :> Post '[JSON] ()
-    :<|> "fortune" :> "trade" :> "player" :> Capture "email" Text :> Get '[JSON] FortunePair
+    :<|> "fortune" :> "trade"
+        :> Capture "email" Text :> Capture "email" Text :> Get '[JSON] FortunePair
 
 data NewPlayer
     = NewPlayer
@@ -67,7 +69,22 @@ main = do
         :<|> tradeFortunes env)
 
 createPlayer :: Environment -> NewPlayer -> EitherT ServantErr IO ()
-createPlayer = undefined
+createPlayer env player = do
+    now <- liftIO getCurrentTime
+    let f = Fortune (npFortune player) now
+    m <- liftIO $ readIORef (serverFortuneMap env)
+    let m' = Map.insert (npEmail player) f m
+    liftIO $ writeIORef (serverFortuneMap env) m'
 
-tradeFortunes :: Environment -> Email -> EitherT ServantErr IO FortunePair
-tradeFortunes = undefined
+tradeFortunes :: Environment -> Email -> Email -> EitherT ServantErr IO FortunePair
+tradeFortunes env from to = do
+    m <- liftIO $ readIORef (serverFortuneMap env)
+    let mgiven    = Map.lookup from m
+        mreceived = Map.lookup to m
+    case (mgiven, mreceived) of
+        (Nothing, Nothing) -> left err404
+        (Just given, Just received) -> do
+            let m'  = Map.update (\_ -> Just received) from m
+                m'' = Map.update (\_ -> Just given) to m'
+            liftIO $ writeIORef (serverFortuneMap env) m''
+            return $ FortunePair given received
