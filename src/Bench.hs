98d8e16a366bba05d6abe200ca8f1ab032bc36c6
AndrewRademacher <andrewrademacher@gmail.com>
2015-11-08 13:25:06 -0600

    Fixed vector indexing property.

--- src/Bench.hs
+++ src/Bench.hs
@@ -12,7 +12,6 @@ import           Servant.Server
 import           System.Random.MWC.Monad    as MWC
 
 import           API
-import qualified Impl.IORef1                as IORef1
 import qualified Impl.IORef2                as IORef2
 import qualified Impl.MVar1                 as MVar1
 import qualified Impl.STM1                  as STM1
@@ -27,7 +26,6 @@ data TestEnvironment
     = TestEnvironment
         { teFortunes  :: Vector NewPlayer
         , teTrades    :: Vector (Email, Email)
-        , teIORef1Env :: IORef1.Environment
         , teIORef2Env :: IORef2.Environment
         , teMVar1Env  :: MVar1.Environment
         , teTVar1Env  :: TVar1.Environment
@@ -36,22 +34,20 @@ data TestEnvironment
 
 main :: IO ()
 main = do
-    te <- MWC.runWithSystemRandom (createOffsetTestEnv 10000 9000)
+    te <- MWC.runWithSystemRandom (createOffsetTestEnv 10000 10000)
     print $ length $ teFortunes te
     print $ length $ teTrades te
     let threads = 8
     defaultMain
         [
             bgroup "create players"
-                [ bench "ioref-1" $ whnfIO $ benchCreate threads (teFortunes te) $ IORef1.createPlayer (teIORef1Env te)
-                , bench "ioref-2" $ whnfIO $ benchCreate threads (teFortunes te) $ IORef2.createPlayer (teIORef2Env te)
+                [ bench "ioref-2" $ whnfIO $ benchCreate threads (teFortunes te) $ IORef2.createPlayer (teIORef2Env te)
                 , bench "mvar-1"  $ whnfIO $ benchCreate threads (teFortunes te) $ MVar1.createPlayer (teMVar1Env te)
                 , bench "tvar-1"  $ whnfIO $ benchCreate threads (teFortunes te) $ TVar1.createPlayer (teTVar1Env te)
                 , bench "stm-1"   $ whnfIO $ benchCreate threads (teFortunes te) $ STM1.createPlayer (teSTM1Env te)
                 ]
         ,   bgroup "make trades"
-                [ bench "ioref-1" $ whnfIO $ benchTrade threads (teTrades te) $ IORef1.tradeFortunes (teIORef1Env te)
-                , bench "ioref-2" $ whnfIO $ benchTrade threads (teTrades te) $ IORef2.tradeFortunes (teIORef2Env te)
+                [ bench "ioref-2" $ whnfIO $ benchTrade threads (teTrades te) $ IORef2.tradeFortunes (teIORef2Env te)
                 , bench "mvar-1"  $ whnfIO $ benchTrade threads (teTrades te) $ MVar1.tradeFortunes (teMVar1Env te)
                 , bench "tvar-1"  $ whnfIO $ benchTrade threads (teTrades te) $ TVar1.tradeFortunes (teTVar1Env te)
                 , bench "stm-1"   $ whnfIO $ benchTrade threads (teTrades te) $ STM1.tradeFortunes (teSTM1Env te)
@@ -87,13 +83,12 @@ slices size l =
 createOffsetTestEnv :: Players -> Trades -> Generator TestEnvironment
 createOffsetTestEnv pcount tcount = do
     v <- V.replicateM pcount newplayerGenerator
-    f <- V.replicateM tcount (MWC.uniformR (0, pcount))
-    t <- V.replicateM tcount (MWC.uniformR (0, pcount))
+    f <- V.replicateM tcount (MWC.uniformR (0, pcount - 1))
+    t <- V.replicateM tcount (MWC.uniformR (0, pcount - 1))
     let ts = V.map (\(fix, tix) -> (npEmail (v V.! fix), npEmail (v V.! tix)))
            $ V.zip f t
     TestEnvironment v ts
-        <$> liftIO IORef1.createEnvironment
-        <*> liftIO IORef2.createEnvironment
+        <$> liftIO IORef2.createEnvironment
         <*> liftIO MVar1.createEnvironment
         <*> liftIO TVar1.createEnvironment
         <*> liftIO STM1.createEnvironment
@@ -103,9 +98,9 @@ newplayerGenerator = NewPlayer <$> textGenerator <*> textGenerator
 
 textGenerator :: Generator Text
 textGenerator = do
-  l <- length'
-  s <- replicateM l char
-  return $! T.pack s
-  where
-    length' = MWC.uniformR (7, 20)
-    char = Char.chr <$> MWC.uniformR (Char.ord 'a', Char.ord 'z')
+    l <- length'
+    s <- replicateM l char
+    return $! T.pack s
+    where
+        length' = MWC.uniformR (7, 20)
+        char = Char.chr <$> MWC.uniformR (Char.ord 'a', Char.ord 'z')
