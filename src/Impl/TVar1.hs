bdd6f324919612ba6d56742709a1ca6cc2a30e00
AndrewRademacher <andrewrademacher@gmail.com>
2015-11-07 19:24:55 -0600

    Added STM implementation 1.

---
+++ src/Impl/TVar1.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Impl.TVar1
+    ( runApp
+    ) where
+
+import           Control.Concurrent.STM
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Either
+import           Data.HashMap.Strict        (HashMap)
+import qualified Data.HashMap.Strict        as Map
+import           Data.Proxy
+import           Data.Time
+import           Network.Wai.Handler.Warp   (run)
+import           Servant.API
+import           Servant.Server
+
+import           API
+
+data Environment
+    = Environment
+        { serverFortuneMap :: TVar (HashMap Email (TVar Fortune))
+        }
+
+runApp :: IO ()
+runApp = do
+    fm <- newTVarIO Map.empty
+    let env = Environment fm
+    run 8080 $ serve (Proxy :: Proxy API)
+        (    createPlayer env
+        :<|> tradeFortunes env)
+
+createPlayer :: Environment -> NewPlayer -> EitherT ServantErr IO ()
+createPlayer env player = do
+    now <- liftIO getCurrentTime
+    liftIO $ atomically $ do
+        f <- newTVar (Fortune (npFortune player) now)
+        m <- readTVar (serverFortuneMap env)
+        let m' = Map.insert (npEmail player) f m
+        writeTVar (serverFortuneMap env) m'
+
+tradeFortunes :: Environment -> Email -> Email -> EitherT ServantErr IO FortunePair
+tradeFortunes env from to = do
+    res <- liftIO $ atomically $ do
+        m <- readTVar (serverFortuneMap env)
+        let mgivenv    = Map.lookup from m
+            mreceivedv = Map.lookup to m
+        case (mgivenv, mreceivedv) of
+            (Just givenv, Just receivedv) -> do
+                given    <- readTVar givenv
+                received <- readTVar receivedv
+                writeTVar givenv received
+                writeTVar receivedv given
+                return $ Just $ FortunePair given received
+            _ -> return Nothing
+    case res of
+        Just v  -> return v
+        Nothing -> left err404
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import qualified Impl.IORef1        as IORef1
 import qualified Impl.IORef2        as IORef2
 import qualified Impl.MVar1         as MVar1
 import qualified Impl.MVar2         as MVar2
+import qualified Impl.TVar1         as TVar1
 
 main :: IO ()
 main = do
@@ -13,5 +14,6 @@ main = do
         "ioref-2" -> IORef2.runApp
         "mvar-1"  -> MVar1.runApp
         "mvar-2"  -> MVar2.runApp
+        "tvar-1"  -> TVar1.runApp
         v         ->
             putStrLn $ "Invalid implementation: " ++ v
