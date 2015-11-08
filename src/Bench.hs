cc0e8e7fe33480860e131d5215a8d9e54d9c806e
AndrewRademacher <andrewrademacher@gmail.com>
2015-11-08 13:14:40 -0600

    Successful bench and output documentation.

--- src/Bench.hs
+++ src/Bench.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 import           Control.Concurrent.Async
-import           Control.DeepSeq
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Either
@@ -11,7 +8,6 @@ import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import           Data.Vector                (Vector)
 import qualified Data.Vector                as V
-import           GHC.Generics
 import           Servant.Server
 import           System.Random.MWC.Monad    as MWC
 
@@ -19,7 +15,6 @@ import           API
 import qualified Impl.IORef1                as IORef1
 import qualified Impl.IORef2                as IORef2
 import qualified Impl.MVar1                 as MVar1
-import qualified Impl.MVar2                 as MVar2
 import qualified Impl.STM1                  as STM1
 import qualified Impl.TVar1                 as TVar1
 
@@ -35,14 +30,15 @@ data TestEnvironment
         , teIORef1Env :: IORef1.Environment
         , teIORef2Env :: IORef2.Environment
         , teMVar1Env  :: MVar1.Environment
-        , teMVar2Env  :: MVar2.Environment
         , teTVar1Env  :: TVar1.Environment
         , teSTM1Env   :: STM1.Environment
         }
 
 main :: IO ()
 main = do
-    te <- MWC.runWithSystemRandom (createOffsetTestEnv 1000000 100000)
+    te <- MWC.runWithSystemRandom (createOffsetTestEnv 10000 9000)
+    print $ length $ teFortunes te
+    print $ length $ teTrades te
     let threads = 8
     defaultMain
         [
@@ -50,7 +46,6 @@ main = do
                 [ bench "ioref-1" $ whnfIO $ benchCreate threads (teFortunes te) $ IORef1.createPlayer (teIORef1Env te)
                 , bench "ioref-2" $ whnfIO $ benchCreate threads (teFortunes te) $ IORef2.createPlayer (teIORef2Env te)
                 , bench "mvar-1"  $ whnfIO $ benchCreate threads (teFortunes te) $ MVar1.createPlayer (teMVar1Env te)
-                , bench "mvar-2"  $ whnfIO $ benchCreate threads (teFortunes te) $ MVar2.createPlayer (teMVar2Env te)
                 , bench "tvar-1"  $ whnfIO $ benchCreate threads (teFortunes te) $ TVar1.createPlayer (teTVar1Env te)
                 , bench "stm-1"   $ whnfIO $ benchCreate threads (teFortunes te) $ STM1.createPlayer (teSTM1Env te)
                 ]
@@ -58,7 +53,6 @@ main = do
                 [ bench "ioref-1" $ whnfIO $ benchTrade threads (teTrades te) $ IORef1.tradeFortunes (teIORef1Env te)
                 , bench "ioref-2" $ whnfIO $ benchTrade threads (teTrades te) $ IORef2.tradeFortunes (teIORef2Env te)
                 , bench "mvar-1"  $ whnfIO $ benchTrade threads (teTrades te) $ MVar1.tradeFortunes (teMVar1Env te)
-                , bench "mvar-2"  $ whnfIO $ benchTrade threads (teTrades te) $ MVar2.tradeFortunes (teMVar2Env te)
                 , bench "tvar-1"  $ whnfIO $ benchTrade threads (teTrades te) $ TVar1.tradeFortunes (teTVar1Env te)
                 , bench "stm-1"   $ whnfIO $ benchTrade threads (teTrades te) $ STM1.tradeFortunes (teSTM1Env te)
                 ]
@@ -66,13 +60,29 @@ main = do
 
 ---
 
-benchCreate :: Word -> Vector NewPlayer -> (NewPlayer -> EitherT ServantErr IO ()) -> IO ()
-benchCreate = undefined
+benchCreate :: Int -> Vector NewPlayer -> (NewPlayer -> EitherT ServantErr IO ()) -> IO ()
+benchCreate scount plist action = do
+    _ <- mapConcurrently runGroup s
+    return ()
+    where
+        s = slices scount (V.toList plist)
+        runGroup = mapM_ (runEitherT . action)
 
-benchTrade :: Word -> Vector (Email, Email) -> (Email -> Email -> EitherT ServantErr IO FortunePair) -> IO ()
-benchTrade = undefined
+benchTrade :: Int -> Vector (Email, Email) -> (Email -> Email -> EitherT ServantErr IO FortunePair) -> IO ()
+benchTrade scount tlist action = do
+    _ <- mapConcurrently runGroup s
+    return ()
+    where
+        s = slices scount (V.toList tlist)
+        runGroup = mapM (\(a, b) -> runEitherT $ action a b)
 
 ---
+
+slices :: Int -> [a] -> [[a]]
+slices size l =
+  case splitAt size l of
+    ([], _) -> []
+    (a, b) -> a : slices size b
 
 createOffsetTestEnv :: Players -> Trades -> Generator TestEnvironment
 createOffsetTestEnv pcount tcount = do
@@ -85,7 +95,6 @@ createOffsetTestEnv pcount tcount = do
         <$> liftIO IORef1.createEnvironment
         <*> liftIO IORef2.createEnvironment
         <*> liftIO MVar1.createEnvironment
-        <*> liftIO MVar2.createEnvironment
         <*> liftIO TVar1.createEnvironment
         <*> liftIO STM1.createEnvironment
 
